854254df7369cb81f377f91d349d876c7bba7815
Omkar gavade <Omkargavade002@gmail.com>
2026-04-08 22:35:34 +0530

    Connect frontend to live backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,4 @@
-// export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://portfolio-backend-5s1x.onrender.com";
+export const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8080"
+    : "https://portfolio-backend-5s1x.onrender.com";
